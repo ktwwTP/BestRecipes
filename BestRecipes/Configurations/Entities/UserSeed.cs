8c4581213559978cfd71586cdd9c475e3536ea20
TAN WEI WEN KAVEN 2401524J <2401524J@student.tp.edu.sg>
2026-01-08 14:30:37 +0800

    Added seed role and user

---
+++ BestRecipes/Configurations/Entities/UserSeed.cs
@@ -0,0 +1,28 @@
+﻿using BestRecipes.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace BestRecipes.Configurations.Entities
+{
+    public class UserSeed : IEntityTypeConfiguration<BestRecipesUser>
+    {
+        public void Configure(EntityTypeBuilder<BestRecipesUser> builder)
+        {
+            var hasher = new PasswordHasher<BestRecipesUser>();
+            builder.HasData(
+            new BestRecipesUser
+            {
+                Id = "3781efa7-66dc-47f0-860f-e506d04102e4",
+                Email = "admin@localhost.com",
+                NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                FirstName = "Admin",
+                LastName = "User",
+                UserName = "admin@localhost.com",
+                NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                EmailConfirmed = true // Set to true, otherwise you won't be able to login
+            }
+            );
+        }
+    }
+}
--- BestRecipes/Data/BestRecipesUser.cs
+++ BestRecipes/Data/BestRecipesUser.cs
@@ -5,5 +5,8 @@ namespace BestRecipes.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class BestRecipesUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
+
